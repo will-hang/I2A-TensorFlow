041ad7757f74c829e6129040e7fab2b28d632310
Ubuntu <cs234@CS234FinalProject.c2mlveg3gfyujm0rkkae0wx4ed.jx.internal.cloudapp.net>
2018-03-13 07:51:31 +0000

    2res code

--- env_model/train.py
+++ env_model/train.py
@@ -80,7 +80,7 @@ class EnvironmentModel(object):
 		# self.loss = 	tf.losses.mean_squared_error(self.next_states, pred_states) + \
 		# 				tf.losses.mean_squared_error(self.rewards, pred_rewards)
 
-		self.loss = tf.losses.mean_squared_error(self.next_frames, pred_frames) + tf.losses.mean_squared_error(self.rewards, pred_rewards)
+		self.loss = config.loss_weight * tf.losses.mean_squared_error(self.next_frames, pred_frames) + (1 - config.loss_weight) * tf.losses.mean_squared_error(self.rewards, pred_rewards)
 		# self.loss = tf.Print(self.loss, [self.next_states, self.pred_state], summarize=15)
 
 		variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, config.scope)
@@ -95,9 +95,11 @@ class EnvironmentModel(object):
 			# extract features
 			conv1 = layers.conv2d(x, 32, 8, stride=2)
 			conv2 = layers.conv2d(conv1, 32, 3)
-			conv3 = layers.conv2d(conv2, 32, 3)
+			conv3 = layers.conv2d(conv2, 32, 3, activation_fn=None)
+			conv3 = tf.nn.relu(conv3 + conv1)
 			conv4 = layers.conv2d(conv3, 32, 3)
-			encoded = conv2 + conv3 + conv4
+			conv5 = layers.conv2d(conv2, 32, 3, activation_fn=None)
+			encoded = tf.nn.relu(conv3 + conv5)
 
 			# predict state
 			pred_frame = layers.conv2d_transpose(encoded, 1, 8, stride=2, activation_fn=None)
@@ -108,14 +110,13 @@ class EnvironmentModel(object):
 			rconv2 = layers.conv2d(rpool1, 32, 3)
 			rpool2 = layers.max_pool2d(rconv1, 2, padding="same")
 			rpool2 = layers.flatten(rpool2)
-			rfc1 = layers.fully_connected(rpool2, 100)
-			pred_reward = layers.fully_connected(rfc1, 1, activation_fn=None)
+			pred_reward = layers.fully_connected(rpool2, 1, activation_fn = None)
 
 			return pred_frame, pred_reward
 
 	def predict(self):
-		pred_state = sess.run([self.pred_state])[0]
-		return pred_state
+		pred_state, pred_reward = sess.run([self.pred_state, self.pred_reward])
+		return pred_state, pred_reward
 
 	def train(self):
 		loss, pred_state, pred_reward, _ = sess.run([self.loss, self.pred_state, self.pred_reward, self.train_op])
@@ -204,7 +205,6 @@ def run(sess, config, env):
 
 	# train loop
 	train_states, train_actions, train_rewards, train_next_states = worker.get_batch(config.data_size, config.n_steps)
-	print(train_rewards)
 	s_mean = np.mean(train_states, axis=0, keepdims=True)
 	train_states = (train_states - s_mean)/255.0
 	train_next_states = (train_next_states - s_mean)/255.0
@@ -218,13 +218,13 @@ def run(sess, config, env):
 	})
 
 	losses = []
+	curr_pred_state = None
 	train_steps = 0
 	while True:
 		train_steps += 1
 		try:
 			loss, pred_state, pred_reward = model.train()
-			print(pred_reward)
-			losses.append(loss)
+			curr_pred_state = pred_state
 			print('Train Step {}: Loss {}'.format(train_steps, loss))
 		except tf.errors.OutOfRangeError:
 			break
@@ -241,13 +241,15 @@ def run(sess, config, env):
 		rewards_placeholder: test_rewards,
 		nf_placeholder: test_next_frames
 	})
-	pred_state = model.predict()
+	pred_state, pred_reward = model.predict()
+	print(pred_reward)
 
 	save_path = saver.save(sess, config.save_ckpt)
 	print("Model saved in path: %s" % save_path)
 	# np.save('../outputs/states', test_states * 255.0 + s_mean)
 	# np.save('../outputs/next_states', test_next_states * 255.0 + s_mean)
 	np.save('../outputs/preds', pred_state * 255.0 + s_mean)
+	np.save('../outputs/train_preds', curr_pred_state * 255.0 + s_mean)
 	np.save('../outputs/losses', losses)
 
 parser = argparse.ArgumentParser(description='A3C')
